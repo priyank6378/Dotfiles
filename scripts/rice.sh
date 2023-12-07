a9a25da944e335c225636e554db6f62038ffc42e
Priyank Agarwal <priyankagarwal6378@gmail.com>
2023-12-07 12:29:29 +0530

    zsh added

--- scripts/rice.sh
+++ scripts/rice.sh
@@ -1,4 +1,4 @@
-sudo apt install bspwm sxhkd rxvt-unicode feh picom polybar mpd 
+sudo apt install bspwm sxhkd rxvt-unicode feh picom polybar mpd alacritty rofi
 mkdir .config
 mkdir ~/.config/bspwm
 mkdir ~/.config/sxhkd
@@ -8,3 +8,6 @@ cp /usr/share/doc/bspwm/examples/bspwmrc ~/.config/bspwm
 cp /usr/share/doc/bspwm/examples/sxhkdrc ~/.config/sxhkd
 cp /etc/xdg/picom.conf ~/.config/picom/
 
+# zsh
+sudo apt instll zsh
+sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
